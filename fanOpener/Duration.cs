d0a3b9efd149cb031d6220bba278dd06ca95fb7e
shadowfelldown <shadowfelldown@gmail.com>
2019-08-30 01:17:46 -0400

    Add Duration class to create dictionary for durations as typsafe enum rather than regular enum

---
+++ fanOpener/Duration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fanOpener
+{
+    public class Duration
+    {
+        public static Duration shortF { get; set; }
+        public static Duration mediumF { get; set; }
+        public static Duration longF { get; set; }
+
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public Duration(int val, string name)
+        {
+            Value = val;
+            Name = name;
+        }
+
+        public static IEnumerable<Duration> List()
+        {
+            // alternately, use a dictionary keyed by value
+            return new[] { shortF, mediumF, longF };
+        }
+
+        public static Duration FromString(string durationString)
+        {
+            return List().Single(r => String.Equals(r.Name, durationString, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static Duration FromValue(int value)
+        {
+            return List().Single(r => r.Value == value);
+        }
+    }
+}
